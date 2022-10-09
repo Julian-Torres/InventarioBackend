ff5dca3bb5c254399c09dbb1e6fac6946c51854d
Julian Torres <julian.torres@est.iudigital.edu.co>
2022-10-09 00:11:20 -0500

    update db

--- DB/db-connection-mongo.js
+++ DB/db-connection-mongo.js
@@ -2,7 +2,7 @@ const mongoose =require ('mongoose');
 
 const getConnection = async() =>{
     try{
-            const url='mongodb://inventario:KsBZyhIMNeTzlHAt@ac-nusbaxw-shard-00-00.wnvxi9b.mongodb.net:27017,ac-nusbaxw-shard-00-01.wnvxi9b.mongodb.net:27017,ac-nusbaxw-shard-00-02.wnvxi9b.mongodb.net:27017/InventarioJS?ssl=true&replicaSet=atlas-5dxop9-shard-0&authSource=admin&retryWrites=true&w=majority';
+            const url='mongodb://inventario:5mQvfDdgtOkf7GvK@ac-nusbaxw-shard-00-00.wnvxi9b.mongodb.net:27017,ac-nusbaxw-shard-00-01.wnvxi9b.mongodb.net:27017,ac-nusbaxw-shard-00-02.wnvxi9b.mongodb.net:27017/InventarioJS?ssl=true&replicaSet=atlas-5dxop9-shard-0&authSource=admin&retryWrites=true&w=majority';
 
             await mongoose.connect(url);
 
